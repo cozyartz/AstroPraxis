dba56cd014ca3a77c73760b6628bbead86796306
Andrea Cozart-Lundin <andrea@cozyartzmedia.com>
2025-05-14 15:46:11 -0400

    config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
+  output: 'static', // ensures Astro builds as a static site (good for Cloudflare Pages)
+  site: 'https://astropraxis.cc', // helps generate full URLs in RSS, sitemaps, etc.
 });
