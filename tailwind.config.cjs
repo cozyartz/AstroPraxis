eb68437d82eafbb9f21043fb9e40668e070ee134
Andrea Cozart-Lundin <andrea@cozyartzmedia.com>
2025-05-15 18:45:34 -0400

    Changed about

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,md}"],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            color: '#e5e7eb', // text-gray-200
+            a: { color: '#818cf8' }, // text-indigo-400
+          },
+        },
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('tailwindcss-animate'),
+  ],
+};
